0874bb5a6689a8070bb5d3a24aedcb75981408f0
Kunal Raj <94804624+kunalraj72@users.noreply.github.com>
2025-10-16 04:30:54 +0530

    Create 39-CombinationSum

---
+++ 39-CombinationSum/39-CombinationSum.cpp
@@ -0,0 +1,27 @@
+// Last updated: 16/10/2025, 04:30:53
+class Solution {
+public:
+ 
+ void findCombination(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
+     if(ind == arr.size()){
+         if(target == 0){
+             ans.push_back(ds);
+         }
+         return;
+     }
+     // picking the element  (same index only target changes)
+     if(arr[ind] <= target){
+         ds.push_back(arr[ind]);
+         findCombination(ind,target-arr[ind],arr,ans,ds);
+         ds.pop_back();
+     }
+     //not picking the element (index incremented by 1 and target remains same)
+     findCombination(ind+1,target,arr,ans,ds);
+ }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findCombination(0, target,candidates,ans,ds);
+        return ans;
+    }
+};
