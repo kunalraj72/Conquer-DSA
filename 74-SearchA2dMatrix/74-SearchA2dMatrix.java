3d6db4f89105b399a20fc5c812bd1ca34b8bdf68
Kunal Raj <94804624+kunalraj72@users.noreply.github.com>
2025-10-16 04:30:07 +0530

    Create 74-SearchA2dMatrix

---
+++ 74-SearchA2dMatrix/74-SearchA2dMatrix.java
@@ -0,0 +1,20 @@
+// Last updated: 16/10/2025, 04:30:07
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int r = 0;
+        int c = n - 1;
+
+        while(r < m && c >= 0){
+            if(matrix[r][c] == target){
+                return true;
+            }else if(matrix[r][c] < target){
+                r++;
+            }else{
+                c--;
+            }
+        }
+        return false;
+    }
+}
