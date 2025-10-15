425c82f10bba9d06f23e8088ab0638d669ba1b92
Kunal Raj <94804624+kunalraj72@users.noreply.github.com>
2025-10-16 04:24:29 +0530

    Create 2507-NumberOfCommonFactors

---
+++ 2507-NumberOfCommonFactors/2507-NumberOfCommonFactors.java
@@ -0,0 +1,12 @@
+// Last updated: 16/10/2025, 04:24:28
+class Solution {
+    public int commonFactors(int a, int b) {
+        int cnt = 0;
+        for(int i = 1; i <= Math.min(a,b); i++){
+            if(a % i == 0 && b % i == 0){
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
