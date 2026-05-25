b7b2c2a28af2774d033cd45b6917e9bff458b31a
Kunal Raj <94804624+kunalraj72@users.noreply.github.com>
2026-05-26 03:44:08 +0530

    Create

---
+++ problem-1779747247255/version/java/problem-1779747247255_20260526_034407.java
@@ -0,0 +1,24 @@
+// Last updated: 26/05/2026, 03:44:07
+1class Solution {
+2    public int longestConsecutive(int[] nums) {
+3        int n = nums.length;
+4        HashMap<Integer, Integer> mp = new HashMap<>();
+5        int cur = 0;
+6        int max = 0;
+7        for(int i = 0; i < n; i++){
+8           mp.putIfAbsent(nums[i], i);
+9        }
+10        int i = 0;
+11        for(int num : mp.keySet()){
+12            if(!mp.containsKey(num - 1)){
+13                cur = 1;
+14                while(mp.containsKey(num + 1)){
+15                    num++;
+16                    cur++;
+17                }
+18            }
+19            max = Math.max(cur, max);
+20        }
+21        return max;
+22    }
+23}
