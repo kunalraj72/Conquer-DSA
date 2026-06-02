ee27f48ebf1dae479e0f4a174e42c4f045fe7c95
Kunal Raj <94804624+kunalraj72@users.noreply.github.com>
2026-06-03 02:05:27 +0530

    Create 8-StringtoInteger(atoi)

---
+++ 8-StringtoInteger(atoi)/version/java/8-StringtoInteger(atoi)_20260603_020526.java
@@ -0,0 +1,39 @@
+// Last updated: 03/06/2026, 02:05:26
+1class Solution {
+2    public int myAtoi(String s) {
+3        int len = s.length();
+4        double num = 0; //To avoid overflow condition as num can be greater that Integer.MAX_VALUE
+5        if(len == 0) return 0;
+6        int i = 0;
+7
+8        //1. skipping whitespace
+9        while(i < len && s.charAt(i) == ' '){
+10            i++;
+11        }
+12        
+13        //edge case
+14        if(i == len){
+15            return 0;
+16        }
+17
+18        //2. sign check
+19        boolean positive = s.charAt(i) == '+';
+20        boolean negative = s.charAt(i) == '-';
+21
+22        if(positive == true || negative == true){
+23            i++;
+24        }
+25        //3. skipping leading zeroes
+26        while(i < len && s.charAt(i) == '0'){
+27            i++;
+28        }
+29
+30        while(i < len && s.charAt(i) >= '0' && s.charAt(i) <= '9'){
+31            num = num * 10 + s.charAt(i) - '0';
+32            i++;
+33        }
+34
+35        num = negative == true ? -num : num;
+36        return (int) num;
+37    }
+38}
