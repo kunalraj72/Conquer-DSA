9f740868876cfb222aba0e0804e5d4354a603646
Kunal Raj <94804624+kunalraj72@users.noreply.github.com>
2026-06-11 01:29:57 +0530

    Create 47-PermutationsII

---
+++ 47-PermutationsII/version/java/47-PermutationsII_20260611_012957.java
@@ -0,0 +1,32 @@
+// Last updated: 11/06/2026, 01:29:57
+1class Solution {
+2
+3    public List<List<Integer>> findPermutations(boolean[] used, int[] nums, List<Integer> ds, List<List<Integer>> ans){
+4
+5        if(ds.size() == nums.length && !ans.contains(ds)){
+6            ans.add(new ArrayList<>(ds));
+7            return ans;
+8        }
+9    
+10        for(int i = 0; i < nums.length; i++){
+11            if(used[i]) continue;
+12
+13
+14            used[i] = true;
+15            ds.add(nums[i]);
+16            findPermutations(used, nums, ds, ans);
+17            ds.remove(ds.size() - 1);
+18            used[i] = false; // un-choose (backtrack)
+19        }
+20
+21        return ans;
+22    }
+23
+24
+25    public List<List<Integer>> permuteUnique(int[] nums) {
+26        List<List<Integer>> ans = new ArrayList<>();
+27        List<Integer> ds = new ArrayList<>();
+28        boolean[] used = new boolean[nums.length];
+29        return findPermutations(used, nums, ds, ans);
+30    }
+31}
