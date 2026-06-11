b9ed2b189d8c5e8e4af6e27d287fcfa16944050d
Kunal Raj <94804624+kunalraj72@users.noreply.github.com>
2026-06-12 02:45:44 +0530

    Create 486-PredicttheWinner

---
+++ 486-PredicttheWinner/version/java/486-PredicttheWinner_20260612_024543.java
@@ -0,0 +1,25 @@
+// Last updated: 12/06/2026, 02:45:43
+1class Solution {
+2
+3    //Watch code with mik video for explanation.
+4    
+5    //In this solution, we are focusing on calculating difference between score scored by player1
+6    //and player 2 by exploring both the option(picking i by p1 and then j by p1) and then
+7    //returning max(pick_i, pick_j), finally if p1_score > p2_score then 
+8    // solve(nums, 0, n - 1) >= 0; this will return true.
+9
+10    public int solve(int[] nums, int i, int j){
+11        //Base condition
+12        if(i == j) return nums[i];
+13        if(i > j) return 0;
+14
+15        int pick_i = nums[i] - solve(nums, i + 1, j);
+16        int pick_j = nums[j] - solve(nums, i, j - 1);
+17        return Math.max(pick_i, pick_j);
+18    }
+19
+20    public boolean predictTheWinner(int[] nums) {
+21        int n = nums.length;
+22        return solve(nums, 0, n - 1) >= 0;
+23    }
+24}
